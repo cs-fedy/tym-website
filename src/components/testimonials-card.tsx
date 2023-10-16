8d47446c24920df4d83f9a265f74bb7f14d84d98
cs-fedy <fedi.abd001@gmail.com>
2023-10-16 09:53:10 -0700

    testimonial done

--- src/components/testimonials-card.tsx
+++ src/components/testimonials-card.tsx
@@ -16,19 +16,27 @@ export default function TestimonialsCard({
   role,
 }: TestimonialsCardProps) {
   return (
-    <div className="flex w-full flex-col space-y-10 rounded-lg border border-junior-gray/10 px-6 py-12">
-      <p className="mt-5 w-full max-w-xl text-lg font-medium leading-10 text-junior-gray">
+    <div className="flex h-full w-full max-w-xs flex-col space-y-10 rounded-lg border border-junior-gray/10 px-6 py-3">
+      <p className="break-words text-justify text-sm font-medium leading-7 text-junior-gray">
         {description}
       </p>
+
       <div className="flex w-full items-center space-x-4">
-        <div className="h-20 w-20 overflow-hidden rounded-full shadow-sm">
-          <Image src={picture} alt={label} width={400} height={400} />
+        <div className="w-16 overflow-hidden rounded-full shadow-sm">
+          <Image
+            className="aspect-square w-full"
+            src={picture}
+            alt={label}
+            width={400}
+            height={400}
+          />
         </div>
+
         <div className="flex flex-col items-start space-y-1">
-          <h4 className="font-bold capitalize leading-tight text-junior-black">
+          <h4 className="text-sm font-bold capitalize leading-tight text-junior-black">
             {fullName}
           </h4>
-          <span className="mt-5 w-full text-lg capitalize leading-7 text-junior-gray">
+          <span className="w-full text-sm capitalize leading-5 text-junior-gray">
             {role}
           </span>
         </div>
--- src/components/testimonials.tsx
+++ src/components/testimonials.tsx
@@ -1,20 +1,48 @@
+import Slider from "./slider"
 import TestimonialsCard from "./testimonials-card"
 
 export default function Testimonials() {
   return (
-    <section className="w-full bg-junior-gray/5 px-36 py-28">
+    <section className="mx-auto w-4/5 py-20">
       <div className="flex w-full flex-col items-center space-y-16">
         <div className="flex flex-col items-center space-y-4">
-          <h2 className="w-full max-w-xl text-center text-4xl font-bold capitalize leading-tight text-junior-black">
+          <h2 className="w-full max-w-sm text-center text-xl font-bold capitalize leading-tight text-junior-black sm:text-2xl md:max-w-lg md:text-3xl">
             Témoignages
           </h2>
-          <p className="mt-5 w-full max-w-3xl text-center text-lg font-medium leading-7 text-junior-gray">
+
+          <p className="mt-5 w-full max-w-md text-center text-base font-medium leading-6 text-junior-gray md:max-w-lg lg:max-w-2xl lg:text-lg lg:leading-7">
             Découvrez les expériences authentiques de nos clients, exprimant
             leur reconnaissance.
           </p>
         </div>
 
-        <div className="flex w-full items-start space-x-10">
+        <div className="block w-full lg:hidden">
+          <Slider>
+            <TestimonialsCard
+              fullName="ahmed ben bouzid"
+              role="cofondateur et CEO opus lab"
+              picture="/images/ahmed-ben-bouzid.jpeg"
+              label="ahmed ben bouzid cofondateur et CEO opus lab"
+              description="Grâce à Tunisian Youth Marketers, Opus Lab a connu une transformation marketing exceptionnelle. Leur expertise a illuminé notre stratégie, propulsant notre startup vers de nouveaux sommets."
+            />
+            <TestimonialsCard
+              fullName="mehdi cherif"
+              role="cofondateur dar al afkar"
+              picture="/images/mehdi-cherif.jpeg"
+              label="mehdi cherif cofondateur dar al afkar"
+              description="Dar Al Afkar a bénéficié de l'expertise remarquable de Tunisian Youth Marketers. Leur partenariat a été décisif pour notre startup, ouvrant des horizons passionnants dans le domaine du marketing."
+            />
+            <TestimonialsCard
+              fullName="samy jaidéne"
+              role="CEO & founder juniors CV"
+              picture="/images/samy-jaidene.jpeg"
+              label="samy jaidéne CEO & founder juniors CV"
+              description="Les compétences de Tunisian Youth Marketers ont été un atout majeur pour Juniors CV. Leur collaboration a dynamisé notre startup, créant une véritable différence dans notre approche marketing."
+            />
+          </Slider>
+        </div>
+
+        <div className="hidden grid-cols-3 items-start gap-10 lg:grid">
           <TestimonialsCard
             fullName="ahmed ben bouzid"
             role="cofondateur et CEO opus lab"
